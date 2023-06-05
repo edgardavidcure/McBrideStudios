6738bf01dd9864cc27a2bb8b727ab321321cb2e2
edgardavidcure <102256702+edgardavidcure@users.noreply.github.com>
2023-06-05 15:49:14 -0700

    built image slider with modal

--- js/app.js
+++ js/app.js
@@ -41,3 +41,41 @@ setTimeout(() => {
     }
 }, 5000);
 
+
+// Open the Modal
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+}
+
+// Close the Modal
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+function plusGallery(n) {
+    const container = document.querySelector(".imageContainer")
+    const pixels = n + 200
+    container.scrollBy(pixels, 0);
+  }
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  
+  slides[slideIndex-1].style.display = "flex";
+}
